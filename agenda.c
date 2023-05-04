a6411cb1722a6a2a78c892a54bdaaf8168076790
angellusj <angela.souza@alunos.ufersa.edu.br>
2023-05-04 18:26:10 +0000

    new

--- agenda.c
+++ agenda.c
@@ -18,28 +18,31 @@ struct agenda{
     int num;
     contatos * contato;
 };
-
+ 
 struct no_lista{
-	Agenda * dado;
-	Node * prox;
+    Agenda * dado;
+    Node * prox;
 };
-
-Node * criar_lista(Agenda dado){
-    Node * inicio = (node *) malloc (sizeof(node));
-	if(inicio == NULL){
-		printf("Erro ao tentar locar memória")
-	}
-    inicio->dado = dado;
-    inicio->prox = NULL;
-    return inicio;
+ 
+Node * criar_lista(Agenda * dado){
+  Node * inicio = (Node *) malloc (sizeof(Node));
+    if(inicio == NULL){
+        printf("Erro ao tentar locar memória");
+    }
+  inicio->dado = dado;
+  inicio->prox = NULL;
+  return inicio;
 }
+
+
  
 int main(void) {
-    Node* inicio = create_list(1);
-    Node* lista = inicio;
+    Agenda * novoDado = (Agenda*) malloc(sizeof(Agenda));
+    Node* lista = criar_lista(novoDado);
     while (lista != NULL) {
-        printf("%d ", lista->dado);
-        lista = lista->prox;
+      //lista->dado->num = 50;
+      printf("%d\n", lista->dado->num);
+      lista = lista->prox;
     }
     return 0;
 }
@@ -47,48 +50,50 @@ int main(void) {
 
 
 
-/*Lista* cria_lista(void){
-	return NULL;
-}
 
+
+
+ 
+/*
+ 
 Lista* lista_insere(Lista* l, Agenda v){
-	Lista* novo = (Lista*) malloc(sizeof(Lista));
-	if (novo == NULL)
+    Lista* novo = (Lista*) malloc(sizeof(Lista));
+    if (novo == NULL)
     {
         printf("ERRO - memoria insuficiente!");
         exit(1);
     }
-	strcpy(v.nome, novo);
-	novo->prox = l;
-	return novo;
+    strcpy(v.nome, novo);
+    novo->prox = l;
+    return novo;
 }
-
+ 
 int lista_vazia(Lista *l){
-	return (l==NULL);
+    return (l==NULL);
 }
-
+ 
 void lista_imprime(Lista*l){
-	Lista* p;
-	for(p = l; p != NULL; p = p->prox){
-		printf(" Info = %s \n", p->dado); //lembrete: consertar erro de leitura do tipo de dado
-	}
+    Lista* p;
+    for(p = l; p != NULL; p = p->prox){
+        printf(" Info = %s \n", p->dado); //lembrete: consertar erro de leitura do tipo de dado
+    }
 }
-
+ 
 Lista * lista_busca(Agenda* elemento, Lista* l){
-	Lista * p;
-	for(p=l; p!=NULL; p=p->prox){
-		if(p->dado == elemento)
-			return p;
-	}
-	return NULL;
+    Lista * p;
+    for(p=l; p!=NULL; p=p->prox){
+        if(p->dado == elemento)
+            return p;
+    }
+    return NULL;
 }
-
+ 
 Lista * lista_retira(Lista*l,  Agenda* v){
     Lista* ant = NULL; 
     Lista* p = l; 
     while(p->dado!=v){
-    	if (p==NULL)
-        	return l; 
+        if (p==NULL)
+            return l; 
         ant = p;
         p = p->prox;
     }
@@ -100,7 +105,7 @@ Lista * lista_retira(Lista*l,  Agenda* v){
     free(p);
     return l;
 }
-
+ 
 void lista_libera(Lista* l){
     Lista* p = l;
     Lista* t;
@@ -110,34 +115,31 @@ void lista_libera(Lista* l){
         p = t;
     }
 }
-
-
-
+ 
+ 
+ 
 Lista* lista_insere_ordenada(Lista * l, Agenda* v){
-	Lista * novo;
-	Lista * ant = NULL;
-	Lista * p =l;
-	while(p!=NULL && p->dado<v){
-		ant = p;
-		p = p->prox;
-	}
-	novo = (Lista*) malloc(sizeof(Lista));
-	novo->dado = v;
-	if(ant ==NULL){
-		novo->prox = l;
-		l = novo;
-	}
-	else {
-		novo->prox = ant-> prox;
-		ant->prox = novo;
-	}
-	return l;
+    Lista * novo;
+    Lista * ant = NULL;
+    Lista * p =l;
+    while(p!=NULL && p->dado<v){
+        ant = p;
+        p = p->prox;
+    }
+    novo = (Lista*) malloc(sizeof(Lista));
+    novo->dado = v;
+    if(ant ==NULL){
+        novo->prox = l;
+        l = novo;
+    }
+    else {
+        novo->prox = ant-> prox;
+        ant->prox = novo;
+    }
+    return l;
 }
-
-
-
-
-/*struct no{
+ 
+struct no{
     Agenda dado;
     No *prox;
 };
@@ -147,8 +149,8 @@ struct lista{
 };
  
 void adiciona(){
-  /*if (lista.inicio == NULL){
+  if (lista.inicio == NULL){
     criar_inicio();
   } 
-}*/
- 
+}
+*/
