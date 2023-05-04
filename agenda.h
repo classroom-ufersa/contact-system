f7f9d7be1b9722151af356143dadfbc5a7cecc98
angellusj <angela.souza@alunos.ufersa.edu.br>
2023-05-04 17:49:24 +0000

    new

--- agenda.h
+++ agenda.h
@@ -5,25 +5,9 @@ typedef struct contact contatos;
  
 typedef struct agenda Agenda;
  
-typedef struct no No;
- 
-typedef struct lista Lista;
+typedef struct no_lista Node;
 
-Lista* cria_lista(void);
 
-Lista* lista_insere(Lista* l, Agenda dado);
-
-int lista_vazia(Lista *l);
-
-void lista_imprime(Lista*l);
-
-Lista * lista_busca(Agenda* elemento, Lista* l);
-
-Lista * lista_retira(Lista*l,  Agenda* v);
-
-void lista_libera(Lista* l);
-
-Lista* lista_insere_ordenada(Lista * l, Agenda* v);
 
 
 
