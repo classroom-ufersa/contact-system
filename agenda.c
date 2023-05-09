eab4cec8a6d749387c973452f21774b40b0ebbde
angellusj <angela.souza@alunos.ufersa.edu.br>
2023-05-08 21:53:36 -0300

    test

--- agenda.c
+++ agenda.c
@@ -187,27 +187,27 @@ void imprime_contato(Node2 * contato){
 }
 
 void adiciona_contato(Node2 * add){ // Revisar
-  int qnt_add, index;
-  Node2 contatoADD;
-  printf("quantidade add: \n");
-  scanf("%d", &qnt_add);
-  for(index = 0; index < qnt_add; index++){
+  int quantidade_adiciona, index;
+  Node2 contatoAdiciona;
+  printf("Quantos contatos deseja adicionar? \n");
+  scanf("%d", &quantidade_adiciona);
+  for(index = 0; index < quantidade_adiciona; index++){
     printf("Nome: \n");
-    editar_char(contatoADD.dado.nome);
+    editar_char(contatoAdiciona.dado.nome);
     printf("Idade: \n");
-    scanf("%u", &contatoADD.dado.idade);
+    scanf("%u", &contatoAdiciona.dado.idade);
     printf("Telefone: \n");
-    scanf("%u", &contatoADD.dado.tel);
-    printf("E-mail: \n");
-    editar_char(contatoADD.dado.email); 
+    scanf("%u", &contatoAdiciona.dado.tel);
+    printf("E-mail: \n\n");
+    editar_char(contatoAdiciona.dado.email); 
     //contatos[*qnt_add] = contatoADD;
     //*contatoADD = contatoADD + 1;
-    printf("Contato adicionado com sucesso!\n");
+    printf("Contato adicionado com sucesso!\n\n");
   }
 }
 
 void remover_contato(Node2 * contatos){
-  int qnt_remove, index, cont, procura = 0;
+  int quantidade_remove, index, cont, procura = 0;
   char* nome = (char*) malloc(MAX_CHAR*sizeof(char));
   if(nome == NULL){
     printf("Erro ao alocar memória");
@@ -218,12 +218,12 @@ void remover_contato(Node2 * contatos){
     printf("Erro ao alocar memória");
     exit(1);
   }
-  printf("Quantos contatos deseja remover?\n");
-  scanf("%d", &qnt_remove);
-  for(index = 0; index < qnt_remove; index++){
-    printf("Dite o nome do contato que deseja remover:\n");
+  printf("Informe a quatidade de contatos que deseja remover: \n");
+  scanf("%d", &quantidade_remove);
+  for(index = 0; index < quantidade_remove; index++){
+    printf("Informe o nome do contato que será removido: \n");
     editar_char(nome);
-    alvo=lista_busca_Contato(nome, contatos, NULL);
+    alvo = lista_busca_Contato(nome, contatos, NULL);
     if (alvo){
       lista_retira_Contato(alvo, nome);
     } else {
@@ -251,22 +251,22 @@ void remover_contato(Node2 * contatos){
 
 void imprimir_contatos_cadastrado(Node * l){ // Imprimir contatos de uma agenda específica
   int cod; 
-  printf("Dite o código da agenda para listar contatos:");
-  scanf("%i",&cod);
+  printf("Informe o código da agenda para listar contatos: \n");
+  scanf("%d", &cod);
   Node * alvo = lista_busca_Agenda(cod, l, NULL);
   Node2 * index;
   for(index = alvo->dado.contato; index != NULL; index = index->prox){
-    printf("Nome: %s\nIdade: %u\nTelefone: %u\nEmail: %s\n", index->dado.nome, index->dado.idade, index->dado.tel, index->dado.email);
+    printf("Nome: %s\nIdade: %u\nTelefone: %lu\nEmail: %s\n\n", index->dado.nome, index->dado.idade, index->dado.tel, index->dado.email);
   }
   //lista_imprime_Contato(lista2);
 }
 
 void editar_contato(Node * l){ // Contato único
   int cod; 
-  printf("Dite o código da agenda para editar um contato:");
-  scanf("%i",&cod);
+  printf("Informe o código da agenda para editar um contato: \n");
+  scanf("%d", &cod);
   Node * alvoAgenda = lista_busca_Agenda(cod, l, NULL);
-  printf("Digite o nome do contato que deseja editar:");
+  printf("Digite o nome do contato que deseja editar: \n");
   char* nome = (char*) malloc(MAX_CHAR*sizeof(char));
   editar_char(nome);
   Node2 * alvoContato = lista_busca_Contato(nome, alvoAgenda->dado.contato, NULL);
@@ -275,22 +275,43 @@ void editar_contato(Node * l){ // Contato único
       printf("Digite a nova idade: \n");
       editar_char(alvoContato->dado.nome);
       printf("Digite o novo número de telefone: \n");
-      scanf("%i", &alvoContato->dado.tel);
+      scanf("%lu", &alvoContato->dado.tel);
       getchar();
       printf("Digite o novo endereço de e-mail: \n");
       editar_char(alvoContato->dado.email);
-      printf("Contato %s atualizado com sucesso!\n", nome);
+      printf("Contato %s atualizado com sucesso!\n\n", nome);
   } else {
     printf("Contato %s não encontrado na agenda.\n", nome);
   }
 }
 
-void buscar_contato(Node * l){
+void buscar_contato(Node2 * m){
+  int name;
+  //printf("Digite o código da agenda que deseja buscar um contato: \n");
+  //scanf("%d",&cod);
+  Node * alvoContato = lista_busca_Contato(name, m, NULL);
+  printf("Digite nome do contato: \n");
+  char * alvo = (char*)malloc(MAX_CHAR*sizeof(char));
+  if(alvo == NULL){
+    printf("Erro ao alocar memória");
+    exit(1);
+  }
+  editar_char(alvo);
+  Node2 * alvoContato = lista_busca_Contato(alvo, alvoContato->dado.contato, NULL);
+  if (alvoContato!=NULL){
+    imprime_contato(alvoContato);
+  } else {
+    printf("Contato não encontrado.\n");
+  }
+}
+
+
+void consultar_contato_em_agenda(Node * l){
   int cod;
-  printf("Digite qual agenda deseja buscar um contato:\n");
-  scanf("%i",&cod);
+  printf("Digite o código da agenda que deseja buscar um contato: \n");
+  scanf("%d",&cod);
   Node * alvoAgenda = lista_busca_Agenda(cod, l, NULL);
-  printf("Digite nome do contato:\n");
+  printf("Digite nome do contato: \n");
   char * alvo = (char*)malloc(MAX_CHAR*sizeof(char));
   editar_char(alvo);
   Node2 * alvoContato = lista_busca_Contato(alvo, alvoAgenda->dado.contato, NULL);
@@ -300,14 +321,12 @@ void buscar_contato(Node * l){
     printf("Contato não encontrado.\n");
   }
 }
-
-/*
-void consultar_contato_em_agenda(){
-  char nome[MAX_CHAR];
+  
+  /*char nome[MAX_CHAR];
   printf("Digite o nome do contato: ");
   fgets(nome, MAX_CHAR, stdin);
   int index, procura = 0;
-  for (index = 0; index < Contatos; index++) {
+  for (index = 0; index < Contato; index++) {
       if (strcmp(Agenda[index].contatos.nome, nome) == 0) {
           procura = 1;
           printf("Nome: %s\n", agenda[index].contatos.nome);
@@ -320,10 +339,11 @@ void consultar_contato_em_agenda(){
     if (!procura) {
         printf("Contato %s não encontrado na agenda.\n", nome);
     }
-}
-*/
-void quantitativo_agenda(){
+}*/
 
+void quantitativo_agenda(Node * l){
+  int index;
+  
 }
 
 //buscando apenas dados de agendas (precisa buscar dados de contatos)
@@ -336,11 +356,11 @@ void quantitativo_agenda(){
 
 //editar o contato ------------- questionar de qual deseja editar
 
-//buscar contato ------------ questionar de qual agenda
+//buscar contato ------------ em todas as agendas
 
-//consultar contato em uma dada agenda ----------------- questionar de qual agenda / talvez criar um arquivo texto separado para essa função
+//consultar contato em uma dada agenda ----------------- questionar de qual agenda 
 
-//consultar quantitativos de agendas ---------------- quantidade de agendas ou quantidade de contatos em uma agenda (?)
+//consultar quantitativos de agendas ---------------- quantidade de agendas
 
 //gnomeSort -------------- duas funções uma pra contato outra para agenda
 
@@ -366,59 +386,6 @@ int main(void) {
   //quantitativo_agenda(Agenda);
 
 
-
-
-
-  //strcpy(novaAgenda.nome,"TESTE1");
-  //novaAgenda.num = 7;
-  //novaAgenda.cod = 235;
-  //lista_insere_Agenda(lista, novaAgenda);
-  // Node* nodaAlvo = lista_busca(novaAgenda.cod, lista);
-  // printf("[1] Valor do Num: %i\nValor do nome: %s\n", nodaAlvo->dado.num, nodaAlvo->dado.nome);
-  // - Inserir o primeiro dado
-  //novaAgenda.num = 9;
-  //novaAgenda.cod = 236;
-  //strcpy(novaAgenda.nome,"TESTE2");
-  //lista_insere_Agenda(lista,novaAgenda);
-  // nodaAlvo = lista_busca(novaAgenda.cod, lista);
-  // printf("[2] Valor do Num: %i\nValor do nome: %s\n", nodaAlvo->dado.num, nodaAlvo->dado.nome);
-  // - Inserir o segundo dado
-  //novaAgenda.num = 11;
-  //novaAgenda.cod = 237;
-  //strcpy(novaAgenda.nome,"TESTE3");
-  //lista_insere_Agenda(lista,novaAgenda);
-  // nodaAlvo = lista_busca(novaAgenda.cod, lista);
-  // printf("[3] Valor do Num: %i\nValor do nome: %s\n", nodaAlvo->dado.num, nodaAlvo->dado.nome);
-  //lista_imprime_Agenda(lista);
-  printf("------------------------------------------------\n");
-  //lista_retira_Agenda(lista,235);
-  //lista_imprime_Agenda(lista);
-  //lista_libera_Agenda(lista);
-
-  printf("\n\n\n");
-
-
-  //Node2 * lista2 = criar_lista_Contato();
-  //contatos novoContato;
-  ///strcpy(novoContato.nome,"TESTE1.2");
-  //novoContato.tel = 19915126639;
-  //novoContato.idade = 19;
-  //lista_insere_Contato(lista2, novoContato);
-  //novoContato.tel = 18625488658;
-  //novoContato.idade = 23;
-  //strcpy(novoContato.nome,"TESTE2.2");
-  //lista_insere_Contato(lista2, novoContato);
-  //novoContato.tel = 11625488658;
-  //novoContato.idade = 23;
-  //strcpy(novoContato.nome,"TESTE3.2");
-  //lista_insere_Contato(lista2, novoContato);
-  //lista_imprime_Contato(lista2);
-  printf("------------------------------------------------\n");
-  // lista_retira_Contato(lista2, 19);
-  //lista_imprime_Contato(lista2);
-  //lista_libera_Contato(lista2);
-
-
   //menu de opções
   return 0;
 }
@@ -429,6 +396,8 @@ int main(void) {
 //-MENU ADICIONAR CONTATO
 //-MENU REMOVER CONTATO
 //-MENU LISTAR CONTATO
+//-MENU EDITAR CONTATO
+//-MENU BUSCAR CONTATO
 
 
 
