c3523ba1cfbc7b82d2e0a3ce1a2d716d90a1a48d
angellusj <123104907+angellusj@users.noreply.github.com>
2023-05-12 13:45:06 -0300

    Update agenda.h

--- agenda.h
+++ agenda.h
@@ -15,31 +15,15 @@ Node *criar_lista_Agenda(void);
 
 Node *lista_busca_Agenda(int elemento, Node *l, Node **anterior);
 
-void lista_retira_Agenda(Node *l, int v);
-
-void lista_insere_Agenda(Node *l, Agenda v);
-
-int lista_vazia_Agenda(Node *l); 
-
-void lista_imprime_Agenda(Node *l, int quantidadeAdiciona); 
-
-void lista_libera_Agenda(Node *l); 
-
-Node2 *criar_lista_Contato(void); 
+void lista_insere_Agenda(Node *l, Agenda v);    
 
 Node2 *lista_busca_Contato(char *element, Node2 *m, Node2 **previous);
 
 void lista_retira_Contato(Node2 *m, char *w);
 
-void lista_insere_Contato(Node2 *m, contatos w);
+void lista_insere_Contato(Node2 *m, contatos w); 
 
-int lista_vazia_Contato(Node2 *m); 
-
-void imprime_contato(Node2 *contato);
-
-void lista_imprime_Contato(Node2 *m); 
-
-void lista_libera_Contato(Node2 *m);
+void imprime_contato(Node2 *contato); 
 
 void limparMemoriaNodes(Node * principio); 
 
